005a757bc139f99c234384c19e8c3f940afd96d8
Victor Carriche <victorcarrichebadillo@gmail.com>
2026-05-19 18:49:09 -0600

    update

--- app/Models/contravel_bd/Cliente.php
+++ app/Models/contravel_bd/Cliente.php
@@ -2,26 +2,38 @@
 
 namespace App\Models\contravel_bd;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
     protected $connection = 'mysql2';
+
+    protected $table = 'clientes';
+
     protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
     protected $fillable = [
         'id_iris',
         'username',
-        'cifrado',
+        'nombres',
+        'apellidos',
         'full_name',
         'email',
         'cve_agencia'
     ];
-    public $timestamps = false;
+
+    protected $casts = [
+        'id_iris' => 'string'
+    ];
 
     public function agency()
     {
-        return $this->belongsTo(Agencia::class, 'cve_agencia');
-        // 👆 asegúrate que la FK se llame agency_id (o cámbiala aquí)
+        return $this->belongsTo(
+            Agencia::class,
+            'cve_agencia',
+            'id_agencia'
+        );
     }
 }
